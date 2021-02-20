11776328a47b0710d66c076793bbe2fafa465704
Ahnaf Tahmid <ahnaf-tahmid@outlook.com>
2021-02-20 18:49:10 +0600

    Create intray topbar with button #2

---
+++ lib/models/global.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color DarkGrayColor = new Color(0xFF212128);
+
+Color redColor = new Color(0xFFE33E57);
+
+TextStyle Intrartytle = new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: DarkGrayColor,
+    fontSize: 40);
